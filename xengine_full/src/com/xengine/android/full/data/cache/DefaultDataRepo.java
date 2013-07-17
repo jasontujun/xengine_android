10df88019616cfab948c525038e8fe0a98c9b16c
tujun <jasontujun@163.com>
2013-07-17 09:53:50 +0800

    v0.52 xengine: add DefaultDataRepo class for easy use

---
+++ xengine_full/src/com/xengine/android/full/data/cache/DefaultDataRepo.java
@@ -0,0 +1,43 @@
+package com.xengine.android.full.data.cache;
+
+
+import java.util.HashMap;
+
+/**
+ * 数据仓库
+ */
+public class DefaultDataRepo implements XDataRepository {
+
+    private static DefaultDataRepo instance;
+
+    public synchronized static DefaultDataRepo getInstance() {
+        if(instance == null) {
+            instance = new DefaultDataRepo();
+        }
+        return instance;
+    }
+
+    public static void clearInstance() {
+        instance = null;
+    }
+
+    private HashMap<String, XDataSource> map;
+    private DefaultDataRepo(){
+        map = new HashMap<String, XDataSource>();
+    }
+
+    @Override
+    public void registerDataSource(XDataSource source) {
+        map.put(source.getSourceName(), source);
+    }
+
+    @Override
+    public void unregisterDataSource(XDataSource source) {
+        map.remove(source.getSourceName());
+    }
+
+    @Override
+    public XDataSource getSource(String sourceName) {
+        return map.get(sourceName);
+    }
+}
